bf77a1d84c2248f5d5a7059d596e10214025154e
ashleysw06 <ashleycmercury@gmail.com>
2026-09-16 17:42:13 -0500

    Now uses ScrollView for note container

--- Umbra Nota/AshleySW06.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <chrono>
-#include "AshleySW06.h"
-
-using namespace std;
-
-string AshleySW06::GetTime() {
-	return "NEVER";
-}
--- Umbra Nota/NotePad.cpp
+++ Umbra Nota/NotePad.cpp
@@ -4,7 +4,9 @@
 #include "wtypes.h"
 #include "TextPrompt.h"
 #include "NotePad.h"
-#include "Container.h"
+#include "Container.h" // UI.h or something
+#include "ScrollView.h" // UI.h or something
+#include "StandardCursor.hpp"
 
 // TODO:
 // - NotePad Styles
@@ -29,10 +31,14 @@ void NotePad::Open() {
     window.setPosition({ desktop.right / 2, desktop.bottom / 2 });
     window.create(VideoMode({ windowWidth, windowHeight }), "Umbra Nota", Style::None);
 
+    StandardCursor normalCursor(StandardCursor::NORMAL);
+    StandardCursor handCursor(StandardCursor::HAND);
+
     // Colors
     Color backgroundColor(30, 30, 45);
     Color panelColor(40, 40, 60);
     Color noteColor(50, 50, 75);
+    Color noteHoveredColor(70, 70, 105);
     Color textColor(200, 200, 200);
 
     // Style & Format
@@ -45,50 +51,55 @@ void NotePad::Open() {
     int headerFontSize = 24;
     int bodyFontSize = 18;
 
-    // Scrolling
-    Vector2i mousePosition;
+    // Note Viewing
+    ScrollView* focusedScrollView{};
+    Note* hoveredNote{};
+    Note* selectedNote{};
 
-    // Scrolling
-    Vector2f scrollDistance = { 0, 0 };
-    Vector2f scrollVelocity = { 0, 0 };
-    float scrollVelocityLimit = 50;
-    float scrollVelocityDrag = 0.9;
-
-    float scrollSpeed = 7;
-    bool limitScrollToContainerBounds = true;
-    float scrollElasticity = 0.9;
-
-    // Containers and (Mostly) Static UI
+    // Containers
     Container body(windowWidth / 2, windowHeight / 2, windowWidth, windowHeight);
 
     UIRect toolBarRect(windowWidth / 2, 15 + defaultPadding, windowWidth - defaultPadding * 2, 30);
     Container toolBar(&toolBarRect);
     RectangleShape toolBarShape = toolBar.GetShape();
-    /*RectangleShape toolUnderlayMaskShape = toolBar.GetShape();
-    toolUnderlayMaskShape.setPosition({ 0.0f, 0.0f });
-    toolUnderlayMaskShape.setSize({ 0.0f + windowWidth, 30 + defaultPadding * 2 });
-    toolUnderlayMaskShape.setFillColor(backgroundColor);*/
     toolBarShape.setFillColor(panelColor);
 
-    UIRect noteContainerRect(windowWidth / 3 + defaultPadding, windowHeight / 2 + defaultPadding + 30, windowWidth * 2 / 3, windowHeight - 30 + defaultPadding * 4);
-    Container noteContainer(&noteContainerRect);
+    UIRect noteContainerRect(
+        windowWidth / 3 + defaultPadding / 3,
+        windowHeight / 2 + (toolBarRect.size.y + defaultPadding * 2) / 2 - (defaultPadding / 2),
+        windowWidth * 2 / 3 - defaultPadding * 3 / 2,
+        windowHeight - (toolBarRect.size.y + defaultPadding * 2) - (defaultPadding)
+    );
+    ScrollView noteContainer(&noteContainerRect, ScrollView::VERTICAL);
     RectangleShape noteContainerShape = noteContainer.GetShape();
     noteContainerShape.setFillColor(panelColor);
 
-    UIRect metadataContainerRect(windowWidth * 0.33, windowHeight + 30, defaultPadding, 0);
-    Container metadataContainer(&metadataContainerRect);
-    RectangleShape metadataContainerShape = metadataContainer.GetShape();
-    metadataContainerShape.setFillColor(panelColor);
+    UIRect dataContainerRect(
+        windowWidth * 5 / 6 + (defaultPadding / 6) - (defaultPadding / 2),
+        windowHeight / 2 + (toolBarRect.size.y + defaultPadding * 2) / 2 - (defaultPadding / 2),
+        windowWidth / 3 - (defaultPadding * 2 / 5) - (defaultPadding),
+        windowHeight - (toolBarRect.size.y + defaultPadding * 2) - (defaultPadding)
+    );
+    Container dataContainer(&dataContainerRect);
+    RectangleShape dataContainerShape = dataContainer.GetShape();
+    dataContainerShape.setFillColor(panelColor);
 
     // Notes
+    vector<Container*> cNotes;
     float noteWidth = noteContainer.rect->size.x - notePadding * 2;
     float noteHeight = fmin(100, fmax(50, noteContainer.rect->size.y)); // Max height fmin[0], Min Height fmax[0]
-
-    Vector2f groupedNoteSize = { noteWidth, noteHeight / 2 + notes.size() * (noteHeight + noteMargin) + notePadding};
-    Vector2f overflowSize = { groupedNoteSize.x - noteContainer.rect->size.x, groupedNoteSize.y - noteContainer.rect->size.y };
+    cout << "Note Base Size: W - " << noteWidth << " H - " << noteHeight;
+    for (size_t i = 0; i < notes.size(); i++) {
+        UIRect cNoteRect(0, 0, noteWidth, noteHeight);
+        Container cNote(&cNoteRect);
+        cNotes.push_back(&cNote);
+        noteContainer.AddChild(&cNote);
+    }
 
     while (window.isOpen()) {
-        scrollVelocity *= scrollVelocityDrag;
+        noteContainer.ApplyDrag();
+        if (noteContainer.rect->Contains(Mouse::getPosition(window))) focusedScrollView = &noteContainer;
+        else focusedScrollView = nullptr;
         while (const optional event = window.pollEvent()) {
             if (event->is<Event::Closed>()) {
                 window.close();
@@ -107,99 +118,86 @@ void NotePad::Open() {
             }
 
             if (const auto* mouseWheelScrolled = event->getIf<sf::Event::MouseWheelScrolled>()) {
-                float lScrollVelocity = 0;
                 switch (mouseWheelScrolled->wheel) {
-                case sf::Mouse::Wheel::Vertical:
-                    lScrollVelocity = scrollVelocity.y + scrollSpeed * mouseWheelScrolled->delta;
-                    scrollVelocity.y = lScrollVelocity;
-                    if (abs(lScrollVelocity) > scrollVelocityLimit) {
-                        scrollVelocity.y = (signbit(lScrollVelocity) ? -1 : 1) * scrollVelocityLimit;
-                    }
-                    break;
                 case sf::Mouse::Wheel::Horizontal:
-                    lScrollVelocity = scrollVelocity.x + scrollSpeed * mouseWheelScrolled->delta;
-                    scrollVelocity.x = lScrollVelocity;
-                    if (abs(lScrollVelocity) > scrollVelocityLimit) {
-                        scrollVelocity.x = (signbit(lScrollVelocity) ? -1 : 1) * scrollVelocityLimit;
-                    }
+                    if (focusedScrollView != NULL) focusedScrollView->AddScrollVelocityX(mouseWheelScrolled->delta);
+                    break;
+                case sf::Mouse::Wheel::Vertical:
+                    if (focusedScrollView != NULL) focusedScrollView->AddScrollVelocityY(mouseWheelScrolled->delta);
                     break;
                 }
-                /*std::cout << "mouse x: " << mouseWheelScrolled->position.x << std::endl;
-                std::cout << "mouse y: " << mouseWheelScrolled->position.y << std::endl;*/
             }
 
-            if (const auto* eMousePosition = event->getIf<sf::Event::MouseMoved>()) {
-                mousePosition = eMousePosition->position;
+            if (const auto* mouseButtonPressed = event->getIf<sf::Event::MouseButtonPressed>()) {
+                if (mouseButtonPressed->button == sf::Mouse::Button::Left) {
+                    if (hoveredNote != NULL) selectedNote = hoveredNote;
+                }
+                if (mouseButtonPressed->button == sf::Mouse::Button::Middle) {
+                    std::cout << "the middle button was pressed" << std::endl;
+                }
+                if (mouseButtonPressed->button == sf::Mouse::Button::Right) {
+                    std::cout << "the right button was pressed" << std::endl;
+                }
             }
         }
-
 
         // Scroll Dist Calculation
-        scrollDistance += scrollVelocity;
-        Vector2f scrollOverflow(-scrollDistance.x - overflowSize.x, -scrollDistance.y - overflowSize.y);
-
-        /*system("cls");
-        std::cout << "Horizontal Axis: X, Width"
-            << "    Scroll Distance: " << scrollDistance.x << endl
-            << "    Base Note Width: " << noteWidth << endl
-            << "    Grouped Note Width: " << groupedNoteSize.x << endl
-            << "    Container Width: " << noteContainer.rect->width << endl
-            << "    Note Overflow: " << overflowSize.x << endl
-            << "    Scroll Overflow: " << scrollOverflow.x << endl << endl
-            << "Vertical Axis: Y, Height" << endl
-            << "    Scroll Distance: " << scrollDistance.y << endl
-            << "    Base Note Height: " << noteHeight << endl
-            << "    Grouped Note Height: " << groupedNoteSize.y << endl
-            << "    Container Height: " << noteContainer.rect->height << endl
-            << "    Note Overflow: " << overflowSize.y << endl
-            << "    Scroll Overflow: " << scrollOverflow.y << endl;*/
-           
-        if (limitScrollToContainerBounds)  {
-            if (scrollDistance.x > 0) scrollDistance.x *= scrollElasticity;
-            if (scrollOverflow.x > 0 && overflowSize.x > 0) {
-                scrollDistance.x = -overflowSize.x - scrollOverflow.x * scrollElasticity;
-            } else if (overflowSize.x < 0) scrollDistance.x *= scrollElasticity;
-
-            if (scrollDistance.y > 0) scrollDistance.y *= scrollElasticity;
-            if (scrollOverflow.y > 0 && overflowSize.y > 0) {
-                scrollDistance.y = -overflowSize.y - scrollOverflow.y * scrollElasticity;
-            } else if (overflowSize.y < 0) scrollDistance.y *= scrollElasticity;
-        }
-
+        noteContainer.ApplyVelocity();
+        hoveredNote = nullptr;
 
         // Start Drawing
+        normalCursor.set(window.getNativeHandle());
         window.clear(backgroundColor);
 
         window.draw(noteContainerShape);
-        window.draw(metadataContainerShape);
+        window.draw(dataContainerShape);
+        window.draw(toolBarShape);
 
         for (size_t i = 0; i < notes.size(); i++) {
             Note* note = &notes[i];
+            UIRect* pTextRect = cNotes[i]->rect;
+            Container* pTextContainer = cNotes[i];
+
+            pTextRect->position.x = noteContainer.rect->position.x + noteContainer.scrollDistance.x;
+            pTextRect->position.y = (noteContainer.rect->GetTop() + noteHeight / 2 + i * (noteHeight + noteMargin) + notePadding) + noteContainer.scrollDistance.y;
+
+            bool isHovered = pTextRect->Contains(Mouse::getPosition(window));
+            if (isHovered) {
+                hoveredNote = note;
+                handCursor.set(window.getNativeHandle());
+            }
 
             RectangleShape noteBody({ noteWidth, noteHeight });
-            UIRect noteRect((noteContainer.rect->position.x) + scrollDistance.x, (noteContainer.rect->GetTop() + noteHeight / 2 + i * (noteHeight + noteMargin) + notePadding) + scrollDistance.y, noteWidth, noteHeight);
-            Container noteDataContainer(&noteRect);
-            Vector2f bodyPosition = { noteDataContainer.rect->GetLeft(), noteDataContainer.rect->GetTop() };
+            noteBody.setPosition({ pTextRect->GetLeft(), pTextRect->GetTop() });
+            noteBody.setFillColor(isHovered ? noteHoveredColor : noteColor);
 
-            noteBody.setPosition(bodyPosition);
-            noteBody.setFillColor(noteColor);
-
-            Text headerText(font, (*note).header, headerFontSize);
+            Text headerText(font, note->header, headerFontSize);
             headerText.setFillColor(textColor);
             headerText.setStyle(sf::Text::Bold);
-            headerText.setPosition({ noteDataContainer.rect->GetLeft() + textPadding, noteDataContainer.rect->GetTop() + textPadding });
+            headerText.setPosition({ pTextRect->GetLeft() + textPadding, pTextRect->GetTop() + textPadding });
 
-            Text bodyText(font, (*note).body, bodyFontSize);
+            Text bodyText(font, note->body, bodyFontSize);
             bodyText.setFillColor(textColor);
-            bodyText.setPosition({ noteDataContainer.rect->GetLeft() + textPadding + bodyTextPadding, noteDataContainer.rect->GetTop() + textPadding * 2 + headerFontSize });
+            bodyText.setPosition({ pTextRect->GetLeft() + textPadding + bodyTextPadding, pTextRect->GetTop() + textPadding * 2 + headerFontSize });
 
             window.draw(noteBody);
             window.draw(bodyText);
             window.draw(headerText);
         }
 
-        //window.draw(toolUnderlayMaskShape);
-        window.draw(toolBarShape);
+        if (selectedNote != NULL) {
+            Text headerText(font, selectedNote->header, headerFontSize);
+            headerText.setFillColor(textColor);
+            headerText.setStyle(sf::Text::Bold);
+            headerText.setPosition({ dataContainer.rect->GetLeft() + textPadding, dataContainer.rect->GetTop() + textPadding });
+
+            Text bodyText(font, selectedNote->body, bodyFontSize);
+            bodyText.setFillColor(textColor);
+            bodyText.setPosition({ dataContainer.rect->GetLeft() + textPadding + bodyTextPadding, dataContainer.rect->GetTop() + textPadding * 2 + headerFontSize });
+
+            window.draw(bodyText);
+            window.draw(headerText);
+        }
 
         window.display();
     }
